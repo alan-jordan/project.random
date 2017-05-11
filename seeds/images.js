f3f3f216b624c2c58629b230fe3b1f860679e476
Alan Jordan <alanpjordan@gmail.com>
2017-05-12 11:48:20 +1200

    Fixed issue in images seed file

--- seeds/images.js
+++ seeds/images.js
@@ -11,8 +11,8 @@ exports.seed = function(knex, Promise) {
         {id: 4, image_url: 'http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg', categories_id: 1},
         {id: 5, image_url: 'https://s-media-cache-ak0.pinimg.com/736x/14/0f/e8/140fe8e50c800327929ea3122d06bb74.jpg', categories_id: 1},
         {id: 6, image_url: '', categories_id: 1},
-        {id: 7, image_url: 'https://betanews.com/wp-content/uploads/2013/12/database.jpg', categories_id: 3,
-        {id: 8, image_url: 'https://tctechcrunch2011.files.wordpress.com/2016/02/cleandatabase.jpg?w=738', categories_id: 3,
+        {id: 7, image_url: 'https://betanews.com/wp-content/uploads/2013/12/database.jpg', categories_id: 3},
+        {id: 8, image_url: 'https://tctechcrunch2011.files.wordpress.com/2016/02/cleandatabase.jpg?w=738', categories_id: 3},
         {id: 9, image_url: 'https://azure.microsoft.com/svghandler/sql-database?width=600&height=315', categories_id: 3},
         {id: 10, image_url: 'http://www.thetravelclub.org/images/travelogues/monopoly/Monopoly.jpg', categories_id: 4},
         {id: 11, image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUpx4drEKZIFiKBuLYZ6pT84i005OunB2qSj7F6pqSyTRqbIVTxg', categories_id: 4},
@@ -22,8 +22,7 @@ exports.seed = function(knex, Promise) {
         {id: 15, image_url: '', categories_id: 5},
         {id: 16, image_url: '', categories_id: 6},
         {id: 17, image_url: '', categories_id: 6},
-        {id: 18, image_url: '', categories_id: 6},
-
+        {id: 18, image_url: '', categories_id: 6}
       ]);
     });
 };
